e22af602a08e5251bc65e8bd6b7c3bbf148b70f1
John Besco <johnbesco.dev@gmail.com>
2022-12-05 12:03:52 +0000

    Create computer choice variable

--- javascript.js
+++ javascript.js
@@ -1,31 +1,39 @@
-let randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+var choices = ['rock', 'paper', 'scissors'];
+
+var computerSelection = choices[Math.floor(Math.random() * choices.length)];
+
+console.log(computerSelection);
+
+/* let randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 
 function getComputerChoice() {
-    if (randomNumber === 1){
+    if (randomNumber == 1){
         return 'Rock';
-    } else if (randomNumber === 2) {
+    } else if (randomNumber == 2) {
         return 'Paper';
-    } else if (randomNumber === 3) {
+    } else if (randomNumber == 3) {
         return 'Scissors';
     }
-}
 
-console.log(getComputerChoice())
+
+    console.log(getComputerChoice())
+}*/
+
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase === computerSelection) {
+    if (playerSelection == computerSelection) {
         return 'Its a draw!';
-    } else if (playerSelection.toLowerCase() == 'rock' && randomNumber === 2) {
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
         return 'You lose! Paper beats Rock!';
-    } else if (playerSelection.toLowerCase() == 'paper' && randomNumber === 1)  {
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock')  {
         return 'You win! Paper beats Rock!';
-    } else if (playerSelection.toLowerCase() == 'paper' && randomNumber === 3) {
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
         return 'You lose! Scissors beats Paper!';
-    } else if (playerSelection.toLowerCase() == 'scissors' && randomNumber === 2) {
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
         return 'You win! Scissors beats Paper!';
-    } else if (playerSelection.toLowerCase() == 'scissors' && randomNumber === 1) {
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
         return 'You lose! Rock beats Scissors!';
-    } else if (playerSelection.toLowerCase() == 'rock' && randomNumber === 3) {
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
         return 'You win! Rock beats Scissors!';
     }
 }
@@ -41,15 +49,16 @@ if player selection = scissors & computer = paper, player wins
 if player selection = scissors & computer = rock, computer wins
 if player selection = rock & computer = scissors, player wins
 */
-console.log(randomNumber)
 const playerSelection = prompt("Choose your weapon!");
-const computerSelection = getComputerChoice();
+console.log(playRound())
 
 
 function game() {
-    for (let i = 0; i < 5; i++){
+    for (let i = 0; i < 20; i++){
         return playRound(playerSelection, computerSelection);
     }
 }
-console.log(game())
+
+console.log(game(playRound(playerSelection, computerSelection)))
+
 
